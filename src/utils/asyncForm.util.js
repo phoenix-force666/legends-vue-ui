b627d06b0655a630a97d129d20dd345183a25b9d
wangqiang <gost.hands@gmail.com>
2020-12-15 15:02:05 +0800

    动态JSON解析器demo

---
+++ src/utils/asyncForm.util.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+// asyncForm =  {
+//      name:'asyncForm',
+//      props:{ },
+//      render:()=>{
+//         return <div>你好</div>
+//      }
+// }
+function AsyncForm(){
+    
+}
+
+export default asyncForm
